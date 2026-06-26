884bd809e6b4ddf7bf53c22cc65d32e1940f61f4
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2026-06-26 09:55:13 -0600

    Revert "fix: revert TV mode to WebSocket + substream (Docker reverse proxy handles WS path, WHEP fails on TV)"
    This reverts commit f9fd23f9e8e159f4b1640c20b7ffefda6838304b.

--- src/app/(tv)/tv/page.tsx
+++ src/app/(tv)/tv/page.tsx
@@ -62,7 +62,7 @@ export default async function TvPage() {
     <KioskGrid
       cameras={cameras}
       cameraFilters={cameraFiltersMap}
-      preferWhep={false}
+      streamType="main"
     />
   );
 }
